e5e16efc39b8c7cf814186513594d6581d2bf3d1
Mitch Myburgh <mitchmyburgh@gmail.com>
2016-03-13 20:05:59 +0200

    added upload form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
+var fileUpload = require('express-fileupload');
 var router = express.Router();
+var app = express();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* get contribute language page */
+router.get('/contribute', function(req, res, next) {
+  res.render('contribute', { title: 'Express' });
+});
+
+// default options
+app.use(fileUpload());
+
+app.post('/upload', function(req, res) {
+	var sampleFile;
+
+	if (!req.files) {
+		res.send('No files were uploaded.');
+		return;
+	}
+
+	sampleFile = req.files.sampleFile;
+	sampleFile.mv('/somewhere/on/your/server', function(err) {
+		if (err) {
+			res.status(500).send(err);
+		}
+		else {
+			res.send('File uploaded!');
+		}
+	});
 });
 
 module.exports = router;
