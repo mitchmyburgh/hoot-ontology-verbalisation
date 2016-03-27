ce25e50731bcae6253a5c5dd074b6ec9d7609dfc
Mitch Myburgh <mitchmyburgh@gmail.com>
2016-03-27 13:38:28 +0200

    added pre built owl files

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,38 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'Hoot', linkToData: ''  });
 });
 
+/* GET pre built owl files. */
+/* GET simpleEnglish */
+router.get('/prebuiltSEnglish', function(req, res, next) {
+  read_file("testont.owl", "sample-owl/testont.owl", "simpleEnglish", function (path){
+    res.render('index', { title: 'Hoot', linkToData: path });
+  });
+});
+/* GET technicalEnglish */
+router.get('/prebuiltTEnglish', function(req, res, next) {
+  read_file("testont.owl", "sample-owl/testont.owl", "technicalEnglish", function (path){
+    res.render('index', { title: 'Hoot', linkToData: path });
+  });
+});
+/* GET maths */
+router.get('/prebuiltMaths', function(req, res, next) {
+  read_file("testont.owl", "sample-owl/testont.owl", "maths", function (path){
+    res.render('index', { title: 'Hoot', linkToData: path });
+  });
+});
+/* GET Tswana */
+router.get('/prebuiltTswana', function(req, res, next) {
+  read_file("tswana.owl", "sample-owl/tswana.owl", "Tswana", function (path){
+    res.render('index', { title: 'Hoot', linkToData: path });
+  });
+});
+/* GET Afrikaans */
+router.get('/prebuiltAfrikaans', function(req, res, next) {
+  read_file("testAfr.owl", "sample-owl/testAfr.owl", "Afrikaans", function (path){
+    res.render('index', { title: 'Hoot', linkToData: path });
+  });
+});
+
 
 /* Post to upload page */
 router.post('/', upload.single('OWLFile'), function (req, res, next) {
